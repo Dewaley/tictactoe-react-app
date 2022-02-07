67265df5b36907454338ef5c86c163b2c161683f
Dewaley <adewaleademilua@gmail.com>
2022-02-07 23:55:31 +0100

    handle click

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import './App.css';
 
 function App() {
+  const xClass = 'x';
+  const oClass = 'o';
+  var oTurn;
+  const cells = document.querySelectorAll('.cell');
+  cells.forEach((cell) => {
+    cell.addEventListener('click', handleClick, { once: true });
+  });
+
+  const handleClick = (e) => {
+    const cell = e.target;
+    const currentClass = oTurn ? oClass : xClass;
+    play(cell, currentClass);
+    console.log(currentClass)
+  };
+  function play(cell, currentClass) {
+    cell.classlist.add(currentClass);
+  };
   return (
-    <div className="App">
-      <div className="scoreboard">
-        <div className="player1"></div>
-        <div className="player2"></div>
+    <div className='App'>
+      <div className='scoreboard'>
+        <div className='player1'></div>
+        <div className='player2'></div>
       </div>
-      <div className="board x">
-        <div className="cell x"></div>
-        <div className="cell o"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
+      <div className='board x'>
+        <div className='cell'></div>
+        <div className='cell'></div>
+        <div className='cell'></div>
+        <div className='cell'></div>
+        <div className='cell'></div>
+        <div className='cell'></div>
+        <div className='cell'></div>
+        <div className='cell'></div>
+        <div className='cell'></div>
       </div>
     </div>
   );
