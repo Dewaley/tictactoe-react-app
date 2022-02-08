4bc00b646cbce8ae593987f05d342935d625042f
Dewaley <adewaleademilua@gmail.com>
2022-02-08 21:27:21 +0100

    hover effect problems

--- src/App.js
+++ src/App.js
@@ -5,22 +5,34 @@ function App() {
   const oClass = 'o';
   var oTurn;
   const cells = document.querySelectorAll('.cell');
+  const board = document.getElementsByClassName('board');
 
   const handleClick = (e) => {
     const cell = e.target;
     const currentClass = oTurn ? oClass : xClass;
-
+console.log(board.classList.add("boy"))
     if (!cell.classList.contains('clicked')) {
       cell.classList.add('clicked');
+      const placeMark = (e) => {
+        cell.classList.add(currentClass);
+      };
+      placeMark();
+      const swapTurns = (e) => {
+        oTurn = !oTurn;
+      };
+      swapTurns();
+      // const setBoardHoverClass = () => {
+        
+      //   // board.classList.remove(xClass);
+      //   // board.classList.remove(oClass);
+      //   // if (oTurn) {
+      //   //   board.classList.add(oClass);
+      //   // } else {
+      //   //   board.classList.add(xClass);
+      //   // }
+      // };
+      // setBoardHoverClass();
     }
-    const placeMark = (e) => {
-      cell.classList.add(currentClass);
-    };
-    placeMark();
-    const swapTurns = (e) => {
-      oTurn = !oTurn
-    }
-    swapTurns()
   };
 
   for (let index = 0; index <= cells; index++) {
@@ -33,7 +45,7 @@ function App() {
         <div className='player1'></div>
         <div className='player2'></div>
       </div>
-      <div className='board x'>
+      <div className='board'>
         <div className='cell' onClick={handleClick}></div>
         <div className='cell' onClick={handleClick}></div>
         <div className='cell' onClick={handleClick}></div>
