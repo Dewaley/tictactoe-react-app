98c7afcafee7c05b54c322dee1d1c311d1221225
Dewaley <adewaleademilua@gmail.com>
2022-02-10 20:43:00 +0100

    cleaner code

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import './App.css';
 import React, { useState } from 'react';
 
 function App() {
-  const [currentClass, setCurrentClass] = useState("x");
+  const [currentClass, setCurrentClass] = useState('x');
   const handleClick = (e) => {
     const cell = e.target;
     if (!cell.classList.contains('clicked')) {
       cell.classList.add('clicked');
       const placeMark = (e) => {
         cell.classList.add(currentClass);
-        if (currentClass === "x") {
-          setCurrentClass('o')
-        } else if (currentClass === "o") {
-          setCurrentClass("x")
-        }
+        const swapTurns = () => {
+          if (currentClass === 'x') {
+            setCurrentClass('o');
+          } else if (currentClass === 'o') {
+            setCurrentClass('x');
+          }
+        };
+        swapTurns();
       };
       placeMark();
       alert(currentClass);
